ff359800a70e3c9a88b8ad7ee6d100571813c04c
Brian Timar <timarbrian@gmail.com>
2019-07-24 22:31:55 -0400

    bug in get_links

--- scrape/tools.py
+++ scrape/tools.py
@@ -1,6 +1,8 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor
 
 def get_html(url):
     """ Returns a string containing the page's html."""
@@ -26,15 +28,21 @@ def get_links(url, domain=None, verbose=False):
         If domain is provided, only those at the given domain (containing it as a substring) 
         are considered."""
     parsed = get_parsed(url)
-    href = re.compile(domain) if (domain is not None) else None
-    links = parsed.find_all('a', href=href)
+    if domain is None:
+        links = parsed.find_all('a')
+    else:
+        href = re.compile(domain)
+        links = parsed.find_all('a', href=href)
     if verbose:
         print("Found {0} links from {1}".format(len(links),url))
     return links
 
+def get_linked_urls(url, domain=None, verbose=False):
+    links = get_links(url, domain=domain, verbose=verbose)
+    return [link['href'] for link in links]
+
 def get_domain_name(url):
     bits = url.split("/")
-    print(bits)
     if 'http' not in bits[0]:
         domain = bits[0]
     else:
@@ -42,7 +50,62 @@ def get_domain_name(url):
     if '.' not in domain:
         raise ValueError("Domain extraction failed for url: {0}".format(url))
     return domain
-    
+
 def travserse_site(baseurl):
     """Crawl through the site with provided base url, returning set of all unique URLs with the same 
-    domain name."""
+    domain name.
+        - for each link on page:
+            if already seen, ignore it
+            otherwise:
+                - add to list of seen
+                - add to queue of links to visit
+        repeat until queue of links to visit is empty
+
+        Based on the queueing example at https://docs.python.org/3/library/queue.html
+        """
+    import threading
+
+    domain = get_domain_name(baseurl)
+    urls_to_visit = Queue()
+    
+    todo = get_linked_urls(baseurl, domain=domain, verbose=False)
+    print(todo)
+    seen = set(todo)
+    modify_seen_lock = threading.Lock()
+
+    for url in todo:
+        urls_to_visit.put(url)
+    
+    def url_processor():
+        while True:
+            url = urls_to_visit.get()
+            if url is None:
+                # 'None' plays the role of a kill signal
+                break
+            new_urls = get_linked_urls(url, domain=domain)
+            with modify_seen_lock:
+                for url in new_urls:
+                    if url not in seen:
+                        urls_to_visit.put(url)
+                        seen.add(url)
+            # tells q that this worker is finished
+            urls_to_visit.task_done()
+
+    num_worker_threads = 10
+    threads = []
+    for __ in range(num_worker_threads):
+        t = threading.Thread(target=url_processor)
+        t.start()
+        threads.append(t)
+    
+    ## block until all candidate urls have been visited
+    urls_to_visit.join()
+    ## kill workers
+    for __ in range(num_worker_threads):
+        urls_to_visit.put(None)
+    for t in threads:
+        t.join()
+    
+    return seen
+
+    
