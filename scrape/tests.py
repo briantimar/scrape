da910fd7459ddbbea342d01b4715f321b19c2956
Brian Timar <timarbrian@gmail.com>
2019-07-24 22:51:11 -0400

    tests

--- scrape/tests.py
+++ scrape/tests.py
@@ -9,5 +9,16 @@ class ToolsTestCase(unittest.TestCase):
         for url, domain in cases:
             self.assertEqual( get_domain_name(url), domain)
 
+    def test_get_protocol(self):
+        from .tools import get_protocol
+        url = "https://example.com"
+        self.assertEqual('https:', get_protocol(url))
+
+    def test_get_base_url(self):
+        from .tools import get_base_url
+        url = "https://example.com/page/other"
+        self.assertEqual("https://example.com", 
+                            get_base_url(url))
+
 if __name__ == "__main__":
     unittest.main()
